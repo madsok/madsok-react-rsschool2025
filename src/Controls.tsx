ee0db6bd521f1ff66e5c0ea89dc6a808470de3c3
madsok <madsok25@gmail.com>
2025-07-14 01:40:40 +0300

    feat: add controls component

---
+++ src/Controls.tsx
@@ -0,0 +1,59 @@
+import { Component } from 'react';
+
+class Controls extends Component<{ onSearchResponse: (data: object) => void }> {
+    state = {
+        searchValue: '',
+    }
+
+    async getData(request:string): Promise<object | null> {
+            try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${request}`);
+
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+                this.props.onSearchResponse(data);
+                return data;
+            } else {
+                console.log(response.status);
+                return null;
+            }
+        } catch (err) {
+            console.log(err);
+            return null;
+        }
+    }
+
+    handleSearchInput = (event: React.ChangeEvent) => {
+        const target = event.target;
+
+        if (target && target instanceof HTMLInputElement) {
+            if (target.value) {
+                this.setState({ searchValue: target.value });
+            } else {
+                this.setState({ searchValue: '' });
+            }
+        }
+
+    }
+
+    handleSearchButton = () => {
+        if (this.state.searchValue || this.state.searchValue == '') {
+            this.getData(this.state.searchValue);
+        }
+    };
+
+    render() {
+        return (
+            <>
+                <h2>Top controls</h2>
+                <div className='controls-wrapper'>
+                    <input className='search-field' type="text" onChange={this.handleSearchInput} />
+                    <button className='search-button' onClick={this.handleSearchButton}>Search Button</button>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Controls;
