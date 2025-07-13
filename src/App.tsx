10685d2e2d1891af6e3905a5fa984bb51f6884fa
madsok <madsok25@gmail.com>
2025-07-14 02:18:32 +0300

    fix: fix method

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css';
 import { Component, type ReactNode } from 'react';
 import Controls from './Controls';
+import Results from './Results';
 
 class App extends Component {
   state = {
     responseData: null,
   };
 
-  updateResponseData(data: object) {
+  updateResponseData = (data: object) => {
     this.setState({ responseData: data });
   }
 
@@ -15,6 +16,7 @@ class App extends Component {
     return (
       <>
         <Controls onSearchResponse={this.updateResponseData} />
+        <Results fetchedData={this.state.responseData ? this.state.responseData : {}}></Results>
       </>
     );
   }
